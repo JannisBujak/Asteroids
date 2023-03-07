f2325dd38f23793c8288ae47e88c213b1ad92dd6
Jannis Bujak <jannis.bujak@grecon.de>
2023-03-07 18:17:55 +0100

    gitignore created

---
+++ QTProject/QTProject.cpp
@@ -0,0 +1,19 @@
+﻿// QTProject.cpp : Defines the entry point for the application.
+//
+
+#include "QTProject.h"
+
+
+using namespace std;
+
+int main(int argc, char** argv)
+{	
+	QApplication app(argc, argv);
+
+	QPushButton button;
+	button.setText("My text");
+	button.setToolTip("A tooltip");
+	button.show();
+
+	return app.exec();
+}
---
+++ QTProject/QTProject.h
@@ -0,0 +1,12 @@
+﻿// QTProject.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <QTCore/QString>
+#include <QtWidgets/QPushbutton>
+#include <QtWidgets/QWidget>
+#include <QtWidgets/QApplication>
+#include <QtGui/QWindow>
+// TODO: Reference additional headers your program requires here.
